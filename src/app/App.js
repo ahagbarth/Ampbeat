c1a8d4ae6c74651f259f8494ffbda6d1328b7149
Alexander Hagbarth <ahagbarth@gmail.com>
2020-05-16 14:44:37 +0200

    Setup

---
+++ src/app/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/App.css';
+import { BrowserRouter as Router,  Route, Switch } from 'react-router-dom'
+
+function App() {
+  return (
+  <Router>
+    
+  </Router>
+  );
+}
+
+export default App;
